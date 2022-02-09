347893ecf08b7540388257dd61b8d09303f0ca8a
masemoel <az.az.az.mjme@gmail.com>
2022-02-09 19:01:26 +0100

    g4: fox_9.0 bringup
    Signed-off-by: masemoel <az.az.az.mjme@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,3 +16,5 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_g4.mk
+
+COMMON_LUNCH_CHOICES := omni_g4-eng
--- omni_g4.mk
+++ omni_g4.mk
@@ -21,6 +21,7 @@ $(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 PRODUCT_PACKAGES += \
 	charger_res_images \
